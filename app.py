397fc6ec83bf0f85e8f357ff50c501dad4aca9dd
Sugali Tharun KUmar <tharunkumaroutai@gmail.com>
2025-11-11 04:47:36 +0530

    Fix Flask host binding for Railway

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def voice_reply():
     return f'<Response><Play>{audio_url}</Play></Response>', 200, {'Content-Type': 'text/xml'}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
